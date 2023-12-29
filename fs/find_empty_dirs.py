0a81a7f5ca3b7a7dfb5858327ca6a4c134af483b
guiweber <guillaume.web@gmail.com>
2023-12-29 12:13:28 +0100

    Allow removing nested directories in a single pass

--- fs/find_empty_dirs.py
+++ fs/find_empty_dirs.py
@@ -13,9 +13,12 @@ if __name__ == "__main__" and __package__ is None:
 
 from shared.utils import print_progress
 
+_scan_count = 0
 
 def find_empty_dirs(lib_path: str, ignore_hidden=False, ignore_size=0, remove='prompt'):
-    """ Finds empty directories and optionally removes them. Small or hidden files can be ignored.
+    """ Finds empty directories and optionally removes them. A directory is considered empty if it contains no or only
+        empty directories, symbolic links and ignored files. The root directory is never removed.
+        Small or hidden files can be ignored. Symbolic links are always ignored.
 
     :param lib_path: Base directory to search from
     :param ignore_hidden: If True, hidden files will be ignored
@@ -27,6 +30,7 @@ def find_empty_dirs(lib_path: str, ignore_hidden=False, ignore_size=0, remove='p
     :return: 0 if the process ran successfully
     """
     # Check the input variables
+    remove = remove.lower()
     if not os.path.isdir(lib_path):
         print("Error - Not a valid directory: " + lib_path)
         return 1
@@ -37,42 +41,67 @@ def find_empty_dirs(lib_path: str, ignore_hidden=False, ignore_size=0, remove='p
     # Check if directories are empty
     msg = "Scanned {} directories"
     empty_dirs = []
-    count = 0
-    for root, dirs, files in os.walk(lib_path):
-        count += 1
-        print_progress(msg, count)
-        if len(dirs) == 0:
-            if len(files) == 0:
-                empty_dirs += [root]
-            elif ignore_hidden or ignore_size:
-                all_ignored = True
-                for file in files:
-                    file_path = os.path.join(root, file)
-                    if not ((ignore_hidden and is_hidden(file, file_path))
-                            or (ignore_size and (os.path.getsize(file_path) < ignore_size))):
-                        all_ignored = False
-                        break
+    if _is_empty_recursive(lib_path, msg, empty_dirs, ignore_hidden, ignore_size):
+        empty_dirs = [entry.path for entry in os.scandir(lib_path) if entry.is_dir(follow_symlinks=False)]
 
-                if all_ignored:
-                    empty_dirs += [root]
-
-    print_progress(msg, count, final=True)
+    print_progress(msg, _scan_count, final=True)
     print()
     if len(empty_dirs):
-        [print(d) for d in empty_dirs]
-        print()
         if (remove == "yes" or
                 (remove == "prompt_all" and input("The above directories are empty, remove? (Y/N)").lower() == "y")):
-            [rmtree(d) for d in empty_dirs]
+            errors = []
+            for d in empty_dirs:
+                try:
+                    rmtree(d)
+                    print(d)
+                except Exception as e:
+                    errors += [str(e)]
+            if len(errors) < len(empty_dirs):
+                print()
+                print("The above directories were empty and have been removed")
+            if errors:
+                print()
+                print("Some errors occurred:")
+                [print(e) for e in errors]
         else:
+            [print(d) for d in empty_dirs]
+            print()
             print("The above directories are empty")
     else:
         print("No empty directories found")
 
 
-def is_hidden(file_name, file_path):
+def _is_empty_recursive(path, msg, empty_dirs, ignore_hidden, ignore_size):
+
+    global _scan_count
+    _scan_count += 1
+    print_progress(msg, _scan_count)
+
+    local_empty_dirs = []
+    has_files = False
+    has_non_empty_dirs = False
+
+    for entry in os.scandir(path):
+        if entry.is_file(follow_symlinks=False) and not has_files:
+            if not ((ignore_hidden and is_hidden(entry))
+                    or (ignore_size and (entry.stat(follow_symlinks=False).st_size < ignore_size))):
+                has_files = True
+        elif entry.is_dir(follow_symlinks=False):
+            if _is_empty_recursive(entry.path, msg, empty_dirs, ignore_hidden, ignore_size):
+                local_empty_dirs += [entry.path]
+            else:
+                has_non_empty_dirs = True
+
+    if has_non_empty_dirs or has_files:
+        empty_dirs += local_empty_dirs
+        return False
+    else:
+        return True
+
+
+def is_hidden(entry):
     """ Returns True if the file is a hidden file, False otherwise """
-    return file_name.startswith('.') or bool(os.stat(file_path).st_file_attributes & stat.FILE_ATTRIBUTE_HIDDEN)
+    return entry.name.startswith('.') or bool(entry.stat(follow_symlinks=False).st_file_attributes & stat.FILE_ATTRIBUTE_HIDDEN)
 
 
 if __name__ == '__main__':
